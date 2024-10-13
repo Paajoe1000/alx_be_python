de5b074f9683744b7e9b836ea84d0d1a9b2a6536
paajoe100 <kojoyankson12@gmail.com>
2024-10-13 20:43:48 +0100

    Add library

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,35 +1,43 @@
 class Book:
 
-    def __init__(self,title,author):
+    def __init__(self,title: str, author: str):
         self.title = title
         self.author = author
 
     def __str__(self):
         return f"{self.__class__.__name__}: {self.title} by {self.author}"
     
+    def __repr__(self):
+        return f"{self.__class__.__name__}: {self.title} by {self.author}"
+    
 class EBook(Book):
-    def __init__(self, title, author, file_size):
+    def __init__(self, title: str, author: str, file_size: int) -> None:
         super().__init__(title, author)
         self.file_size = file_size
     def __str__(self):
-        return f"{super().__str__()}, File Size: {self.file_size}"
+        return f"{super().__str__()}, File Size: {self.file_size}KB"
+    def __repr__(self):
+        return f"{super().__str__()}, File Size: {self.file_size}KB"
     
 class PrintBook(Book):
-    def __init__(self, title, author, page_count):
+    def __init__(self, title, author, page_count: int) -> None:
         super().__init__(title, author)
         self.page_count = page_count
     def __str__(self):
         return f"{super().__str__()}, Page Count: {self.page_count}"
+    def __repr__(self):
+         return f"{super().__str__()}, Page Count: {self.page_count}"
     
-class Library(Book):
-    def __init__(self, books):
+class Library:
+    def __init__(self, name_of_library: str = "") -> None:
+        self.name = name_of_library
         self.books = []
 
-    def add_book(self, book):
+    def add_book(self, book: Book) -> None:
         if book not in self.books:
             self.books.append(book)
 
-    def list_books(self):
+    def list_books(self) -> None:
         if self.books:
             for item in self.books:
                 print(item)
