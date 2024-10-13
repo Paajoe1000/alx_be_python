1fc2b778a52ac7c1f863c36bcbc7d8218b1346f4
paajoe100 <kojoyankson12@gmail.com>
2024-10-13 17:23:44 +0100

    Add library

---
+++ oop/library_system.py
@@ -0,0 +1,34 @@
+class Book:
+
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title)
+        super().__init__(author)
+        self.file_size = int(file_size)
+    def __str__(self):
+        return f"{self.title} by {self.author}, {self.file_size}"
+    
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title)
+        super().__init__(author)
+        self.page_count = int(page_count)
+    def __str__(self):
+        return f"{self.title} by {self.author}, {self.page_count}"
+    
+class Library(Book):
+    def __init__(self, books):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        self.books()
